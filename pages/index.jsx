291816951d7396e7a71fe327e23aff8eaaee51a3
whylock <shawnlockhart7@gmail.com>
2022-01-06 23:30:58 -0600

    Error message

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,14 +7,20 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
 	const [query, setQuery] = useState("facebook");
-	const { data } = useSWR(`/api/orgs/${query}`, fetcher);
+	const { data, error } = useSWR(`/api/orgs/${query}`, fetcher);
 	return (
 		<>
 			<div className="p-20 flex justify-around">
 				<h1 className="text-3xl font-bold">Github Organizations</h1>
 				<Search query={query} setQuery={setQuery} data={data} />
 			</div>
-			<div className="p-6">{data ? <Table data={data} /> : null}</div>
+			<div className="p-6 w-full">
+				{data?.languages ? (
+					<Table data={data} />
+				) : (
+					<h2 className="mx-auto text-2xl text-red-500">{data?.message} Try Another Organization</h2>
+				)}
+			</div>
 		</>
 	);
 }
